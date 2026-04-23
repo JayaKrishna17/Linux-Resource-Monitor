903106d9354c840f26a49e5add53179ce998ba49
jaya <kittujkr@gmail.com>
2026-04-23 17:59:07 +0530

    New changes

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+
+import tkinter as tk
+from gui.dashboard import ResourceMonitorApp
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ResourceMonitorApp(root)
+    root.geometry("700x400")
+    root.mainloop()
+
+# test code or test comment
+# my changes
